fe555dbfb21368b62ed74cac9fcf58bc073f0041
Sam Brieden <sam.brieden@gmail.com>
2021-08-24 21:26:38 -0600

    spiff-exercise added methods to control progress

--- src/progress_bar_exercise/ProgressBar.js
+++ src/progress_bar_exercise/ProgressBar.js
@@ -19,6 +19,33 @@ export default function ProgressBar({ isLoading, breaks = [] }) {
         transitionDelay: !isLoading ? '2700ms' : '0s',
     };
     const handleLoading = useCallback(() => {
+        const resetDuration = () => {
+            setTimeout(() => setTransitionDuration(breakPointDuration), 100);
+        };
+        const handleNextBreakPoint = (i) => {
+            if (timeoutID) clearTimeout(timeoutID);
+            setProgress(breakPoints[i]);
+            if (i < breakPoints.length - 1) {
+                setTimeoutID(
+                    setTimeout(() => {
+                        handleNextBreakPoint(i + 1);
+                    }, breakPointDuration)
+                );
+            }
+        };
+        const handleProgressTermination = () => {
+            setTransitionDuration(1000);
+            setProgress(100);
+            if (timeoutID) clearTimeout(timeoutID);
+            setTimeoutID(
+                setTimeout(() => {
+                    setProgress(0);
+                    setTransitionDuration(0);
+                    setTimeoutID(null);
+                    resetDuration();
+                }, 2900)
+            );
+        };
         if (isLoading && !progress) {
             if (timeoutID) clearTimeout(timeoutID);
             handleNextBreakPoint(0);
