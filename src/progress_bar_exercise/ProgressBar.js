a3c5d6d02070402fd375f38ed26ab0c9136c9a53
Sam Brieden <sam.brieden@gmail.com>
2021-08-24 21:22:43 -0600

    spiff-exercise added various states and styles

--- src/progress_bar_exercise/ProgressBar.js
+++ src/progress_bar_exercise/ProgressBar.js
@@ -3,6 +3,21 @@ import './progress-bar.scss';
 
 export default function ProgressBar({ isLoading, breaks = [] }) {
     const breakPoints = [...breaks, 90];
+    breakPoints.sort();
+    const maxDuration = 15000;
+    const breakPointDuration = maxDuration / breakPoints.length;
+    const [timeoutID, setTimeoutID] = useState(null);
+    const [progress, setProgress] = useState(0);
+    const [transitionDuration, setTransitionDuration] =
+        useState(breakPointDuration);
+    const progressStyles = {
+        width: `${100 - progress}%`,
+        transitionDuration: `${transitionDuration}ms`,
+    };
+    const showProgressBar = {
+        opacity: isLoading ? '100%' : '0%',
+        transitionDelay: !isLoading ? '2700ms' : '0s',
+    };
 
     return (
         <div className="progress-bar-container" style={showProgressBar}>
